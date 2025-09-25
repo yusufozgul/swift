23318f609547d425f23bbab968e6d04592c6b994
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-09-25 13:27:59 +0300

    make alloc and swift change

--- stdlib/public/core/String.swift
+++ stdlib/public/core/String.swift
@@ -374,6 +374,10 @@ public struct String {
   @inlinable @inline(__always)
   @_semantics("string.init_empty")
   public init() { self.init(_StringGuts()) }
+
+  public init(yusuf: String) {
+    self.init("[yusuf]" + yusuf)
+  }
 }
 
 extension String: Sendable { }
