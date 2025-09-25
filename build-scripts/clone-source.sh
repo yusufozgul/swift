5ea05d995d9b49777be2b0c4954fda4426c15adc
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-09-25 10:07:36 +0300

    update clone script

--- build-scripts/clone-source.sh
+++ build-scripts/clone-source.sh
@@ -5,7 +5,7 @@ set -u
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-mkdir -p "$HOME/Documents/swift-build"
+mkdir -p "$HOME/Documents/swift-build" || true
 cd "$HOME/Documents/swift-build"
 
 git clone --depth 1 --branch swift-6.1.2-RELEASE-CustomDevelopments https://github.com/yusufozgul/swift swift
