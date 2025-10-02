60bec8a04bbcad01c7276152de09a476a0942a53
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-10-02 22:37:28 +0300

    update build preset

--- build-scripts/build-toolchain.sh
+++ build-scripts/build-toolchain.sh
@@ -29,21 +29,115 @@ fi
 cat >> "$TEMP_PRESET_FILE" << 'EOF'
 
 # Custom preset: iOS-only build for faster compilation
-[preset: buildbot_osx_package,no_test,ios_only]
-mixin-preset=buildbot_osx_package,no_test
+[preset: custom_ios_only_package]
+mixin-preset=mixin_buildbot_install_components_with_clang
 
-skip-build-tvos
-skip-build-watchos
-skip-build-xros
+# ONLY macOS and iOS - no tvOS, watchOS, xrOS
+ios
+
+# Build all the necessary tools
+lldb
+llbuild
+swiftpm
+swift-driver
+swiftsyntax
+swift-testing
+swift-testing-macros
+swiftformat
+playgroundsupport
+indexstore-db
+sourcekit-lsp
+swiftdocc
+
+# Build configuration
+release-debuginfo
+compiler-vendor=apple
+
+# Assertions (lightweight)
+assertions
+swift-enable-ast-verifier=0
+no-swift-stdlib-assertions
+
+lldb-use-system-debugserver
+lldb-build-type=Release
+build-ninja
+build-swift-stdlib-unittest-extra
 
 # Disable embedded stdlib to avoid armv6 serialization crash
 build-embedded-stdlib=0
+
+# Don't build the benchmarks
+skip-build-benchmarks
+
+# CMake options
+extra-cmake-options=
+   -DLLDB_FRAMEWORK_COPY_SWIFT_RESOURCES=0
+   -DCMAKE_C_FLAGS="-gline-tables-only"
+   -DCMAKE_CXX_FLAGS="-gline-tables-only"
+
+extra-dsymutil-args="--verify-dwarf=none"
+
+# Install components
+install-llvm
+install-static-linux-config
+install-swift
+install-lldb
+install-llbuild
+install-swiftpm
+install-swift-driver
+install-swiftsyntax
+install-swift-testing
+install-swift-testing-macros
+install-playgroundsupport
+install-sourcekit-lsp
+install-swiftformat
+install-swiftdocc
+
+install-destdir=%(install_destdir)s
+darwin-install-extract-symbols
+install-symroot=%(install_symroot)s
+install-prefix=%(install_toolchain_dir)s/usr
+
+test-installable-package
+toolchain-benchmarks
+reconfigure
+
+installable-package=%(installable_package)s
+symbols-package=%(symbols_package)s
+
+darwin-toolchain-bundle-identifier=%(darwin_toolchain_bundle_identifier)s
+darwin-toolchain-display-name=%(darwin_toolchain_display_name)s
+darwin-toolchain-display-name-short=%(darwin_toolchain_display_name_short)s
+darwin-toolchain-name=%(darwin_toolchain_xctoolchain_name)s
+darwin-toolchain-version=%(darwin_toolchain_version)s
+darwin-toolchain-alias=%(darwin_toolchain_alias)s
+darwin-toolchain-require-use-os-runtime=0
+
+build-subdir=buildbot_osx
+
+# Skip all tests (including LLDB - rdar://67923799)
+skip-test-swift
+skip-test-swiftpm
+skip-test-swift-driver
+skip-test-llbuild
+skip-test-lldb
+skip-test-cmark
+skip-test-playgroundsupport
+skip-test-swiftsyntax
+skip-test-swiftformat
+skip-test-skstresstester
+skip-test-swiftdocc
+skip-test-sourcekit-lsp
+skip-test-indexstore-db
+
+# Don't configure LLDB tests to avoid building libcxx (rdar://109774179)
+lldb-configure-tests=0
 EOF
 
 ./utils/build-script \
     --sccache \
     --preset-file=$TEMP_PRESET_FILE \
-    --preset="buildbot_osx_package,no_test,ios_only" \
+    --preset="custom_ios_only_package" \
     install_destdir="$INSTALL_DIR" \
     install_prefix="swift-LOCAL-a.xctoolchain/usr" \
     install_symroot="$WORK_DIR/symbols" \
